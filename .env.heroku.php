07685355ef0c25caf4e473d096c45700a0a2bd14
Graham Campbell <graham@mineuk.com>
2014-12-29 19:09:38 +0000

    CS fixes

--- .env.heroku.php
+++ .env.heroku.php
@@ -3,15 +3,10 @@
 $dbURL = parse_url(getenv('CLEARDB_DATABASE_URL'));
 $dbName = substr($dbURL["path"], 1);
 
-return array(
-
+return [
     'DB_HOST' => $dbURL['host'],
     'DB_DATABASE' => $dbName,
     'DB_USERNAME' => $dbURL['user'],
     'DB_PASSWORD' => $dbURL['pass'],
     'DB_DRIVER' => 'mysql',
-
-);
-
-
-
+];
